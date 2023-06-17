5841b31d4471805eae7cc56e4bded5e2655f21c7
Ivan Cavero <ivangonzalezcavero@gmail.com>
2023-06-17 14:00:16 +0200

    ✨ Add warning icon when user no have connection and app shows storage data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,30 @@
 // src/components/Header.js
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useState, useEffect } from 'react'
 import { LinearGradient } from 'expo-linear-gradient';
-import { TouchableOpacity } from 'react-native';
 import MenuIcon from './Icons/MenuIcon'
+import WarningIcon from './Icons/WarningIcon'
+import { subscribeToConnectionChanges } from '../utils/checkInternetConnection';
+import { Tooltip } from 'react-native-elements';
 
 const Header = () => {
+  const [isConnected, setIsConnected] = useState(true);
+
+  useEffect(() => {
+    const checkConnection = async () => {
+      const unsubscribe = subscribeToConnectionChanges((connected) => {
+        setIsConnected(connected);
+      });
+
+      return () => {
+        unsubscribe();
+      };
+    };
+
+    checkConnection();
+  }, []);
+  
   return (
     <LinearGradient
       colors={['#60A3D9', '#0074B7']}
@@ -14,13 +33,27 @@ const Header = () => {
       style={styles.container}
     >
       <Text style={styles.title}>Go4Launch</Text>
-      <TouchableOpacity style={styles.menuIcon}>
-        <MenuIcon
-          size={24}
-        />
-      </TouchableOpacity>
+      <View style={styles.iconsContainer}>
+      {!isConnected && (
+          <TouchableOpacity style={styles.warningIcon}>
+            <Tooltip
+              popover={<Text style={{ color: "#fff" }}>You are in offline mode. The data may not be up to date.</Text>}
+              backgroundColor="rgb(61, 61, 61)"
+              height={60}
+              width={200}
+              overlayColor="rgba(0, 0, 0, 0)"
+            >
+              <WarningIcon size={24} fill={'#f54242'} />
+            </Tooltip>
+          </TouchableOpacity>
+        )}
+        <TouchableOpacity style={styles.menuIcon}>
+          <MenuIcon size={24} />
+        </TouchableOpacity>
+      </View>
     </LinearGradient>
   );
+  
 };
 
 
@@ -41,6 +74,13 @@ const styles = StyleSheet.create({
   menuIcon: {
     padding: 5,
   },
+  iconsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  warningIcon: {
+    marginRight: 10
+  }
 });
 
 
