b59373012a99b39c64a3601300e2eb0394d5f264
Ivan Cavero <ivangonzalezcavero@gmail.com>
2023-07-24 19:56:09 +0200

    ✨ Add new page Configuration and store default theme

--- App.jsx
+++ App.jsx
@@ -1,21 +1,36 @@
 import React from "react";
 import { useFonts } from "expo-font";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, DefaultTheme, DarkTheme } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
-import { View, Text } from "react-native";
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
+import { View, Text, useColorScheme } from "react-native";
+
+import { configureStore } from '@reduxjs/toolkit';
+import configurationReducer, { setTheme} from './src/store/configuration';
+import { Provider, useDispatch } from 'react-redux';
 
 import UpcomingLaunchList from "./src/components/Launches/UpcomingLaunchList";
 import RocketIcon from "./src/components/Icons/RocketIcon";
 import PastLaunchList from "./src/components/Launches/PastLaunchList";
 import HistoryIcon from "./src/components/Icons/HistoryIcon";
 import Header from "./src/components/Header/Header";
+import ConfigurationView from "./src/components/Configuration/ConfigurationView";
 
 const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
 
+const store = configureStore({
+  reducer: {
+    configuration: configurationReducer,
+  },
+});
+
 const App = () => {
+  const scheme = useColorScheme();
+  const dispatch = useDispatch();
+  dispatch(setTheme(scheme));
+
   const [fontsLoaded] = useFonts({
     "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
   });
@@ -48,20 +63,22 @@ const App = () => {
 
   const HomeScreen = ({ navigation }) => (
     <Tab.Navigator swipeEnabled={true} screenOptions={createScreenOptions}>
-      <Tab.Screen name="Home" component={UpcomingLaunchList} options={{ header: () => <Header navigation={navigation} /> }} />
-      <Tab.Screen name="Favorites" component={PastLaunchList} options={{ header: () => <Header navigation={navigation} /> }} />
+      <Tab.Screen name="Upcoming" component={UpcomingLaunchList} options={{ header: () => <Header navigation={navigation} /> }} />
+      <Tab.Screen name="Past" component={PastLaunchList} options={{ header: () => <Header navigation={navigation} /> }} />
     </Tab.Navigator>
   );
+
+  const ConfigurationScreen = ({ navigation }) => (
+    <React.Fragment>
+      <Header navigation={navigation} />
+      <ConfigurationView />
+    </React.Fragment>
+  );  
 
   const CustomDrawerContent = (props) => (
     <DrawerContentScrollView {...props} contentContainerStyle={{ flex: 1 }}>
       <HeaderDrawer />
       <DrawerItemList {...props} />
-      <DrawerItem
-        label="Configuration"
-        onPress={() => props.navigation.closeDrawer()}
-        disabled={true}
-      />
       <View style={{ flex: 1 }}></View>
       <FooterDrawer />
     </DrawerContentScrollView>
@@ -85,16 +102,21 @@ const App = () => {
 
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
+      <NavigationContainer theme={scheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Drawer.Navigator
           drawerContent={(props) => <CustomDrawerContent {...props} />}
           screenOptions={{ headerShown: false }}
         >
           <Drawer.Screen name="Home" component={HomeScreen} />
+          <Drawer.Screen name="Configuration" component={ConfigurationScreen} />
         </Drawer.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
   );
 };
 
-export default App;
+export default () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
