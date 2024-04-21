a9eb7710f987064f51030d01a2bdd70833f73812
Ivan Cavero <ivangonzalezcavero@gmail.com>
2024-04-21 18:13:09 +0200

    L4F-27 - Update app.json, babel.config.js, and eas.json

---
+++ app.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  name: process.env.APP_NAME,
+  slug: 'Launch4Fun',
+  scheme: 'Launch4Fun',
+  version: '0.0.0',
+  orientation: 'portrait',
+  icon: process.env.ICON_PATH,
+  userInterfaceStyle: 'automatic',
+  splash: {
+    image: process.env.SPLASH_IMAGE_PATH,
+    resizeMode: 'contain',
+    backgroundColor: '#242424'
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: process.env.ADAPTIVE_ICON,
+      backgroundColor: '#242424'
+    }
+  },
+  web: {
+    favicon: process.env.FAV_ICON
+  },
+  extra: {
+    eas: {
+      projectId: '8c84f868-f7f4-4d64-b238-9c1d04c436da'
+    },
+    API_URL: process.env.API_URL,
+    API_URL_ANALYTICS: process.env.API_URL_ANALYTICS,
+    APP_NAME: process.env.APP_NAME,
+    APP_DOMAIN: process.env.APP_DOMAIN
+  }
+}
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,25 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = (api) => {
+  api.cache(true)
   return {
-    presets: ['babel-preset-expo'],
-  };
-};
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@Navigation": "./src/navigation",
+            "@Components": "./src/components",
+            "@Styles": "./src/styles",
+            "@Utils": "./src/utils",
+            "@Hooks": "./src/hooks",
+            "@Store": "./src/store",
+            "@Services": "./src/services",
+            "@Assets": "./assets"
+          },
+          extensions: [".js", ".jsx"]
+        }
+      ]
+    ]
+  }
+}
