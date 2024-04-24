7456b2e2b7337dc520637968830ab50087c8dfd1
Ivan Cavero <ivangonzalezcavero@gmail.com>
2024-04-24 16:35:42 +0200

    Update LaunchListItem component with date formatting, countdown, and status styling

--- src/components/Launches/Item/LaunchListItem.js
+++ src/components/Launches/Item/LaunchListItem.js
@@ -3,74 +3,10 @@ import useTheme from '@/styles/useTheme'
 import { formatDateToLocal } from '@/utils/dateUtils'
 import { useCountdown } from '@/hooks/useCountdown'
 import { getStatusBackgroundColor, getStatusText } from '@/utils/statusUtil'
+import { setSelectedLaunch } from '@Store/selectedLaunch'
 
 export default function LaunchListItem({ launch }) {
   const appTheme = useTheme()
-  const windowWidth = Dimensions.get('window').width
-  const isDesktop = Platform.OS === 'web'
-  const gridSize = isDesktop ? windowWidth / 4 - 20 : ''
-
-  const styles = StyleSheet.create({
-    itemContainer: {
-      width: gridSize,
-      flexDirection: 'row',
-      alignItems: 'center',
-      marginVertical: 5,
-      borderRadius: 8,
-      backgroundColor: appTheme.bg200,
-      ...Platform.select({
-        android: {
-          elevation: 2
-        },
-        ios: {
-          shadowColor: '#000',
-          shadowOffset: { width: 0, height: 1 },
-          shadowOpacity: 0.8,
-          shadowRadius: 1
-        }
-      })
-    },
-    infoContainer: {
-      flex: 1,
-      flexDirection: 'column',
-      justifyContent: 'space-between',
-      padding: 15
-    },
-    itemName: {
-      fontSize: 14,
-      color: appTheme.text100,
-      marginBottom: 5
-    },
-    itemNet: {
-      fontSize: 12,
-      color: appTheme.primary100
-    },
-    dateAndCountdown: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      marginBottom: 5
-    },
-    countdown: {
-      fontSize: 10,
-      color: appTheme.text200
-    },
-    launchStatus: {
-      marginLeft: 10,
-      paddingVertical: 1,
-      paddingHorizontal: 4,
-      borderRadius: 7
-    },
-    launchStatusText: {
-      fontSize: 8,
-      color: '#212529'
-    },
-    itemImage: {
-      width: 80,
-      height: '100%',
-      borderTopRightRadius: 10,
-      borderBottomRightRadius: 10
-    }
-  })
 
   const formattedDate = formatDateToLocal(launch.net)
   const statusBackgroundColor = getStatusBackgroundColor(launch.status?.name)
@@ -78,20 +14,82 @@ export default function LaunchListItem({ launch }) {
   const countdown = useCountdown(launch.net)
 
   return (
-    <View style={styles.itemContainer}>
+    <View style={[styles.itemContainer, { backgroundColor: appTheme.bg200 }]}>
       <View style={styles.infoContainer}>
-        <Text style={styles.itemName} numberOfLines={1}>
+        <Text style={[styles.itemName, { color: appTheme.text100 }]} numberOfLines={1}>
           {launch.name}
         </Text>
         <View style={styles.dateAndCountdown}>
-          <Text style={styles.itemNet}>{formattedDate}</Text>
+          <Text style={[styles.itemNet, { color: appTheme.primary100 }]}>{formattedDate}</Text>
           <View style={[styles.launchStatus, { backgroundColor: statusBackgroundColor }]}>
             <Text style={styles.launchStatusText}>{statusText}</Text>
           </View>
         </View>
-        <Text style={styles.countdown}>{countdown}</Text>
+        <Text style={[styles.countdown, { color: appTheme.text200 }]}>{countdown}</Text>
       </View>
       <Image source={{ uri: launch.image }} style={styles.itemImage} />
     </View>
   )
 }
+
+const windowWidth = Dimensions.get('window').width
+const isDesktop = Platform.OS === 'web'
+const gridSize = isDesktop ? windowWidth / 4 - 20 : ''
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    width: gridSize,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 5,
+    borderRadius: 8,
+    ...Platform.select({
+      android: {
+        elevation: 2
+      },
+      ios: {
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.8,
+        shadowRadius: 1
+      }
+    })
+  },
+  infoContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    padding: 15
+  },
+  itemName: {
+    fontSize: 14,
+    marginBottom: 5
+  },
+  itemNet: {
+    fontSize: 12
+  },
+  dateAndCountdown: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 5
+  },
+  countdown: {
+    fontSize: 10
+  },
+  launchStatus: {
+    marginLeft: 10,
+    paddingVertical: 1,
+    paddingHorizontal: 4,
+    borderRadius: 7
+  },
+  launchStatusText: {
+    fontSize: 8,
+    color: '#212529'
+  },
+  itemImage: {
+    width: 80,
+    height: '100%',
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10
+  }
+})
